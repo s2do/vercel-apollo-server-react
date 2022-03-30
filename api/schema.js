0871809517b87a573fe5b68578b9e74c4a4dba95
Santiago Soler Perez Olaya <santiago.soler_perez_olaya@tu-dresden.de>
2022-03-30 09:04:58 +0200

    new schema

--- api/schema.js
+++ api/schema.js
@@ -17,11 +17,54 @@ export const typeDefs = gql`
 
 	type Query {
 		beasts: [Beast]
+		cmfs:	[CMF]
+	}
+
+	type CMF {
+		"Goal of the CMF"
+		goal: Expression
+		"Output of the CMF"
+		output: [Expression]
+		"Input of the CMF"
+		input: [Expression]
+		"Constraint of the CMF"
+		contraint:	[Expression]
+		"Representations"
+		representation:	[Representation]
+		"Related CMFs"
+		related:	[{CMF, Expression}]
+	}
+
+	type Expression {
+		"List of terms"
+		terms:	[Term]
+	}
+
+	type Term {
+		"Standard semantic URI"
+		uri: String
+		"Generality group"
+		generality: Generality
+	}
+
+	type Generalilty {
+		"Name of the generality group"
+		name: String
+		"Parent generality group"
+		parent: Generality
+	}
+
+	type Representation {
+		"Management system for this representation"
+		managementSystem:	String
+		"Script/Implementation"
+		script:	String
 	}
 `
 export const resolvers = {
 	Query: {
 		// Returns array of all beasts.
 		beasts: () => db.beasts,
+		cmfs:	() => db.cmfs,
 	}
 }
